1c47d546e73f4f2935cb36ea29ba705999262d4d
w3modabbir <modabbirhossen.web@gmail.com>
2024-02-09 12:32:36 +0600

    react setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3>Hello React</h3>
+    </>
+  )
+}
+
+export default App
