2d114a150f23ad1973e8106994900f678109502a
w3modabbir <modabbirhossen.web@gmail.com>
2024-02-09 15:45:35 +0600

    google fonts, html input form, css done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import { useState } from 'react'
-
+import './style.css'
+import { getDatabase, ref, set } from "firebase/database";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const db = getDatabase();
+  const [item, setItem] = useState("")
+
+  const handleForm = (e) =>{
+      setItem(e.target.value);
+  }
+  const hendleAdd = (e) =>{
+    e.preventDefault();
+    set(ref(db, "alltodo"), {
+      todoText: item,
+    });
+    
+  }
+
 
   return (
     <>
-      <h3>Hello React</h3>
+      {/*=============== todo part start here ================= */}
+        <div className='todo_main'>
+          <header>
+            <h2>Todo List </h2>
+          </header>
+          <form action="#" method='post'>
+            <input onChange={handleForm} type="text"  placeholder='Enter your text'/>
+            <button onClick={hendleAdd}>add</button>
+          </form>
+        </div>
+      {/*=============== todo part end ======================== */}
     </>
   )
 }
---
+++ src/config/firebaseCofig.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCztX_9nVaaC38L-_xTJWFoUzcpQgr3QUU",
+  authDomain: "my-chating-a4c9e.firebaseapp.com",
+  projectId: "my-chating-a4c9e",
+  storageBucket: "my-chating-a4c9e.appspot.com",
+  messagingSenderId: "1000761861317",
+  appId: "1:1000761861317:web:a7846537144af80fe9bcac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig
